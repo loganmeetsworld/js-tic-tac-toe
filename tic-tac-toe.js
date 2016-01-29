3837d1809c12d2fcaab159ce8cc8be9cff83f408
Logan McDonald <loganmcdona11@gmail.com>
2016-01-29 09:36:19 -0800

    added helpful comments

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,19 @@
 function TicTacToe() {
+  // constants throuhgout game
   this.binary_wins = [7, 56, 448, 73, 146, 292, 273, 84];
-  this.turn = "O";
-  this.boxes = [["tl"], ["tm"], ["tr"], ["ml"], ["mm"], ["mr"], ["bl"], ["bm"], ["br"]]; 
+  this.current_turn = "O";
 
+  // box logic
+  this.boxes = [["tl"], ["tm"], ["tr"], ["ml"], ["mm"], ["mr"], ["bl"], ["bm"], ["br"]]; 
   var count = 1;
   this.boxes.forEach(function(box){
     box.push(count);
     count = count * 2;
   })
 
+  // things to be reset in new game
   this.moves = 0;
   this.score = {"X": 0, "O": 0};
-
   this.newGame();
   this.selectSpace();
   this.winningScore();
@@ -19,11 +21,11 @@ function TicTacToe() {
 }
 
 TicTacToe.prototype = {
+  // setting variables back to zero 
   newGame: function(){
     var self = this;
     $(".new-game").click(function () {
-      console.log("HELLO");
-      self.turn = "O";
+      self.current_turn = "O";
       self.moves = 0;
       self.score = {"X": 0, "O": 0};
       self.boxes.forEach(function(box) {
@@ -32,14 +34,15 @@ TicTacToe.prototype = {
     });
   },
 
+  // selecting a square click logic
   selectSpace: function(){
     var self = this;
     self.boxes.forEach(function(box) {
       $("#" + box).click(function () {
         if ($(this).is(':empty')){
-          $(this).append('<h1>' + self.turn + '</h1>');
+          $(this).append('<h1>' + self.current_turn + '</h1>');
           self.moves ++;
-          self.score[self.turn] += box[1];
+          self.score[self.current_turn] += box[1];
           this.onclick = self.checkEnvironment();;
         } else {
           alert("Already selected!") 
@@ -48,10 +51,11 @@ TicTacToe.prototype = {
     });
   }, 
 
+  // checking the condition of the board after each turn
   checkEnvironment: function(){
     var self = this; 
-    if (self.winningScore(self.score[self.turn])) {
-      alert("Player " + self.turn + " wins!");
+    if (self.winningScore(self.score[self.current_turn])) {
+      alert("Player " + self.current_turn + " wins!");
       self.newGame();
       $(".new-game").click()
     } else if (self.moves === 9) {
@@ -59,10 +63,11 @@ TicTacToe.prototype = {
       self.newGame();
       $(".new-game").click()
     } else {
-      self.turn = self.turn === "X" ? "O" : "X";
+      self.current_turn = self.current_turn === "X" ? "O" : "X";
     }
   },
 
+  // checking to see if O or X have a winning score
   winningScore: function(score){
     var self = this;
     for (i = 0; i < self.binary_wins.length; i += 1) {
